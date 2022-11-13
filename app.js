c0322f908213c8d72596dc4494aca41dee0c710d
Nick <ntoukas.nick@gmail.com>
2022-11-13 15:10:40 -0500

    project complete

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 //using selectors inside the element method
+const questions = document.querySelectorAll('.question');
 
-// traversing the dom method
-const btns = document.querySelectorAll('.question-btn');
+questions.forEach((question) => {
+  const btn = question.querySelector('.question-btn');
 
-btns.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    const question = e.currentTarget.parentElement.parentElement;
+  btn.addEventListener('click', () => {
+    questions.forEach((q) => {
+      if (q !== question) {
+        q.classList.remove('show-text');
+      }
+    });
+
     question.classList.toggle('show-text');
   });
 });
+
+// traversing the dom method
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach((btn) => {
+//   btn.addEventListener('click', (e) => {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle('show-text');
+//   });
+// });
